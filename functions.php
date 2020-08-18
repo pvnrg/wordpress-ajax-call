0f0b70d75b11f9194f8093e59f914d8e1053c984
Pavan Ganvani <45678301+pvnrg@users.noreply.github.com>
2020-08-18 11:16:33 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -76,6 +76,7 @@ function register_testimonials_module() {
 		'public'                => true,
 		'show_ui'               => true,
 		'show_in_menu'          => true,
+		'show_in_rest'		=> true,	// To use Gutenberg editor.
 		'menu_position'         => 5,
 		'show_in_admin_bar'     => true,
 		'show_in_nav_menus'     => true,
